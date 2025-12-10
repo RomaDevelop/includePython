def0d11db278905f4da1a538d9aeaab3bd24d527
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-12-10 14:31:43 +0300

    Update AnyGuard

--- any_guard.py
+++ any_guard.py
@@ -1,12 +1,17 @@
 class AnyGuard:
-    def __init__(self, var, enter_value=True, exit_value=False):
-        self.var = var
+    def __init__(self, setter_func, enter_value, exit_value):
+        self.setter_func = setter_func
         self.enter_value = enter_value
         self.exit_value = exit_value
 
     def __enter__(self):
-        self.var = self.enter_value   # Устанавливаем новое значение
+        self.setter_func(self.enter_value)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.var = self.exit_value     # Устанавливаем значение при выходе
+        self.setter_func(self.exit_value)
+
+# в питоне нельзя передать в функцию bool по ссылке, поэтому только setter_func
+# нельзя вот так (с переменной): def __init__(self, var, enter_value, exit_value)  ...  self.var = self.enter_value
+
+# можно сделать ещё класс, который вместо вызова функции с начальным и конечным значением вызывает две разные функции
